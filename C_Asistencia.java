e71397243791a0d9ac7153cd9ab1a6913952ce61
Jeferson <jefersonmencosc7@gmail.com>
2025-08-26 20:32:20 -0600

    Update C_Asistencia.java

--- C_Asistencia.java
+++ C_Asistencia.java
@@ -1,16 +1,18 @@
 package service;
+
 import lombok.ToString;
-
+import lombok.Getter;
 import java.util.*;
-public class C_Asistencia {
-    @lombok.ToString
 
+@ToString
+@Getter
+public class C_Asistencia {
     private List<C_Estudiante> estudiantes;
 
     public C_Asistencia() {
         estudiantes = new LinkedList<>();
     }
-    public void addEstudiante (C_Estudiante estudiante){
+    public void addEstudiante(C_Estudiante estudiante) {
         estudiantes.add(estudiante);
     }
 }
