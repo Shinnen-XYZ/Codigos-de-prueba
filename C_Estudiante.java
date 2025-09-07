e52622335d62b6b5604c6483cc5dc5fdf62758ce
Jeferson <jefersonmencosc7@gmail.com>
2025-09-07 16:43:53 -0600

    Update C_Estudiante.java

--- C_Estudiante.java
+++ C_Estudiante.java
@@ -4,6 +4,10 @@ public class C_Estudiante {
     private int carnet;
 
     public C_Estudiante(){
+        this.carnet = 0;
+    }
+
+    public C_Estudiante(int carnet){
         this.carnet = carnet;
     }
 
@@ -14,7 +18,19 @@ public class C_Estudiante {
     public void setCarnet(int carnet) {
         this.carnet = carnet;
     }
+
     public String toString(){
-        return "C_Estudiante" + carnet;
+        return "C_Estudiante{carnet=" + carnet + "}";
+    }
+}
+
+class MyException extends Exception {
+    
+    public MyException(String mensaje) {
+        super(mensaje);
+    }
+    
+    public String toString() {
+        return "MyException: " + getMessage();
     }
 }
