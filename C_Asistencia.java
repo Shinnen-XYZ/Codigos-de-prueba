a2be4b253cbde512caa0fdc32420fb4e33df7ef5
Jeferson <jefersonmencosc7@gmail.com>
2025-09-07 14:15:02 -0600

    Add files via upload

--- C_Asistencia.java
+++ C_Asistencia.java
@@ -9,13 +9,28 @@ import java.util.*;
 @Setter
 @ToString
 public class C_Asistencia {
+
+
     private List<C_Estudiante> estudiantes;
 
     public C_Asistencia() {
-        estudiantes = new LinkedList<>();
+        estudiantes = new LinkedList();
     }
 
-    public void addEstudiante(C_Estudiante estudiante) {
-        estudiantes.add(estudiante);
+    public void addEstudiante(C_Estudiante e) throws RuntimeException{
+        if( null == e )
+            throw new RuntimeException("No se permiten estudiantes vacios");
+        estudiantes.add(e);
     }
+
+    public List<C_Estudiante> getEstudiantes() {
+        return new LinkedList(estudiantes);
+    }
+
+    public void setEstudiantes(List<C_Estudiante> estudiantes) {
+        for (C_Estudiante cEstudiante : this.estudiantes = estudiantes) {
+        }
+    }
+
+
 }
--- C_Estudiante.java
+++ C_Estudiante.java
@@ -1,45 +1,20 @@
 package service;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.*;
+public class C_Estudiante {
+    private int carnet;
 
-public class C_AsistenciaTest {
-
-    @Test
-    public void testGetterEstudiantes() {
-        C_Asistencia asistencia = new C_Asistencia();
-        assertNotNull(asistencia.getEstudiantes());
-        assertEquals(0, asistencia.getEstudiantes().size());
+    public C_Estudiante(){
+        this.carnet = carnet;
     }
 
-    @Test
-    public void testSetterEstudiantes() {
-        C_Asistencia asistencia = new C_Asistencia();
-        List<C_Estudiante> lista = new ArrayList<>();
-        lista.add(new C_Estudiante());
-        asistencia.setEstudiantes(lista);
-
-        assertEquals(1, asistencia.getEstudiantes().size());
+    public int getCarnet() {
+        return carnet;
     }
 
-    @Test
-    public void testAddEstudiante() {
-        C_Asistencia asistencia = new C_Asistencia();
-        C_Estudiante estudiante = new C_Estudiante();
-        asistencia.addEstudiante(estudiante);
-
-        assertEquals(1, asistencia.getEstudiantes().size());
+    public void setCarnet(int carnet) {
+        this.carnet = carnet;
     }
-
-    @Test
-    public void testToString() {
-        C_Asistencia asistencia = new C_Asistencia();
-        asistencia.addEstudiante(new C_Estudiante());
-        String resultado = asistencia.toString();
-
-        assertNotNull(resultado);
-        assertTrue(resultado.contains("estudiantes"));
-        assertTrue(resultado.contains("C_Estudiante"));
+    public String toString(){
+        return "C_Estudiante" + carnet;
     }
 }
